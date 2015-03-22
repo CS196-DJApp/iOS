4cbb97bc8d5c9393dffc7aa891372e830e991f44
Ryan <rgates4960@gmail.com>
2015-03-22 01:28:01 -0500

    Basic ground work for location services, getting user location, and populating table accordingly

--- CS 196 DJ App/DJ App/BarsNearbyView.swift
+++ CS 196 DJ App/DJ App/BarsNearbyView.swift
@@ -7,27 +7,70 @@
 //
 
 import UIKit
+import CoreLocation
 
 class BarsNearbyView: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
     //color scheme = black background and blue text
-    @IBOutlet
-    var tableView: UITableView!
-    var items : [String]  = ["Kam's", "Murphy's", "Brothers", "Red Lion", "Firehaus"]
+    @IBOutlet var tableView: UITableView!
+    var items2 : [String]  = ["Kam's", "Murphy's", "Brothers", "Red Lion", "Firehaus"]
+    var items = [String]()
     var barName : String!
+    var barLocation : [String : CLLocation!] = [
+        "Kam's" : CLLocation(latitude:40.1079598, longitude:-88.2276094),
+        "Brothers": CLLocation(latitude:40.110126, longitude:-88.229688),
+        "Joe's" : CLLocation(latitude:40.1103156, longitude:-88.2349688),
+        "Legends": CLLocation(latitude:40.1099865, longitude:-88.2296344),
+        "The Clybourne": CLLocation(latitude:40.1099865, longitude:-88.2296344),
+        "The Red Lion": CLLocation(latitude:40.1103156, longitude:-88.2349688),
+        "Murphy's": CLLocation(latitude:40.110492, longitude:-88.230168),
+        "Firehaus": CLLocation(latitude:40.109680, longitude:-88.230264),
+        "White Horse Inn": CLLocation(latitude:40.109245, longitude:-88.230881)]
+    
+    var locationManager : CLLocationManager!
+    var location : CLLocation!
     
     override func viewDidLoad(){
         super.viewDidLoad()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.tableView.backgroundColor = UIColor.darkGrayColor()
         self.tableView.tableFooterView = UIView(frame: CGRectZero)
-        var locationManager = CLLocationManager()
+        
+        locationManager = CLLocationManager()
         locationManager.delegate = self
-        locationManager.startMonitoringSignificantLocationChanges()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 50
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        //var initLocation = CLLocation(latitude: 40.1079598, longitude: -88.2276094)
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!){
+//        println("Latitude: \(locationManager.location.coordinate.latitude)  Longitude:\(locationManager.location.coordinate.longitude)")
+        println("Called and \(locations[locations.count-1])")
+        for (bar,barLoc) in barLocation {
+            println("Bar:\(bar)")
+            println("Distance:\(locationManager.location.distanceFromLocation(barLoc))")
+            if locationManager.location.distanceFromLocation(barLoc) < 2000{
+                items.append(bar)
+                self.tableView.reloadData()
+            }
+            else if locationManager.location.distanceFromLocation(barLoc) > 2000{
+                for var i = 0; i < items.count; i+=1{
+                    if items[i] == "bar"{
+                        items.removeAtIndex(i)
+                    }
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject?) {
         if (segue.identifier == "BarsNearby") {
@@ -57,9 +100,6 @@ class BarsNearbyView: UIViewController, UITableViewDataSource, UITableViewDelega
         self.performSegueWithIdentifier("BarsNearby", sender: self)
     }
     
-    func locationManger(manager: CLLocationManager){
-        
-    }
     
     func loginViewShowingLoggedOutUser(loginView: FBLoginView){
         self.performSegueWithIdentifier("MainScreen", sender : self)
