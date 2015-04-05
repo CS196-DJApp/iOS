15e392a7b32e5beba13eee4042ab468912225b8f
Ryan <rgates4960@gmail.com>
2015-04-05 15:51:28 -0500

    Location services are now integrated
    I would have simply merged the location-services branch into the
    master, but I could not figure out the storyboard merge conflict, so I
    copied and pasted the code.

--- CS 196 DJ App/DJ App/BarsNearbyView.swift
+++ CS 196 DJ App/DJ App/BarsNearbyView.swift
@@ -7,27 +7,85 @@
 //
 
 import UIKit
+import CoreLocation
 
 class BarsNearbyView: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
-    //color scheme = black background and blue text
-    @IBOutlet
-    var tableView: UITableView!
-    var items : [String]  = ["Kam's", "Murphy's", "Brothers", "Red Lion", "Firehaus"]
+    @IBOutlet var tableView: UITableView!
+    var items2 : [String]  = ["Kam's", "Murphy's", "Brothers", "Red Lion", "Firehaus"]
+    var items = [String]()
     var barName : String!
+    var barLocation : [String : CLLocation!] = [ //dictionary of all campus bars and their coordinates
+        "Kam's" : CLLocation(latitude:40.1079598, longitude:-88.2276094),
+        "Brothers": CLLocation(latitude:40.110126, longitude:-88.229688),
+        "Joe's" : CLLocation(latitude:40.1103156, longitude:-88.2349688),
+        "Legends": CLLocation(latitude:40.1099865, longitude:-88.2296344),
+        "The Clybourne": CLLocation(latitude:40.1099865, longitude:-88.2296344),
+        "The Red Lion": CLLocation(latitude:40.1103156, longitude:-88.2349688),
+        "Murphy's": CLLocation(latitude:40.110492, longitude:-88.230168),
+        "Firehaus": CLLocation(latitude:40.109680, longitude:-88.230264),
+        "White Horse Inn": CLLocation(latitude:40.109245, longitude:-88.230881)]
+    
+    var locationManager : CLLocationManager!
+    var location : CLLocation!
+    
+    var distanceIndicator = [UILabel]()
     
     override func viewDidLoad(){
         super.viewDidLoad()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         self.tableView.backgroundColor = UIColor.darkGrayColor()
         self.tableView.tableFooterView = UIView(frame: CGRectZero)
-        var locationManager = CLLocationManager()
+        
+        locationManager = CLLocationManager()
         locationManager.delegate = self
-        locationManager.startMonitoringSignificantLocationChanges()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 50
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        //var initLocation = CLLocation(latitude: 40.1079598, longitude: -88.2276094)
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!){
+        
+        //println("Called and \(locations[locations.count-1])")
+        var distanceString : String
+        var distance : Double
+        var spacesToAdd : Int
+        
+        for (bar,barLoc) in barLocation {
+            
+            //println("Bar:\(bar)")
+            distance = locationManager.location.distanceFromLocation(barLoc)
+            //println("Distance:\(locationManager.location.distanceFromLocation(barLoc))")
+            
+            if distance < 2000{
+                distanceString = String(format: "%.2f mi away", (distance/1000.0)*0.621371) //converts km to mi and formats
+                var label = UILabel()
+                label.text = distanceString
+                items.append(bar)
+                distanceIndicator.append(label)
+                self.tableView.reloadData()
+            }
+            else if locationManager.location.distanceFromLocation(barLoc) > 2000{
+                for var i = 0; i < items.count; i+=1{
+                    if items[i] == "bar"{
+                        self.tableView.beginUpdates()
+                        items.removeAtIndex(i)
+                        //self.tableView.deleteRowsAtIndexPaths(items[i], withRowAnimation: UITableViewRowAnimation.Fade)
+                    }
+                }
+                self.tableView.endUpdates()
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject?) {
         if (segue.identifier == "BarsNearby") {
@@ -45,10 +103,14 @@ class BarsNearbyView: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell : UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
         cell.textLabel?.text = self.items[indexPath.row]
+        cell.addSubview(distanceIndicator[indexPath.row])
+        distanceIndicator[indexPath.row].frame = CGRectMake(240.0, 5.0, 115.0, 30.0)
+        distanceIndicator[indexPath.row].textColor = UIColor(red: 5.0/255.0, green: 208.0/255.0, blue: 255.0/255.0, alpha: 1.0)
         //cell.textLabel?.textAlignment = NSTextAlignment.Center
         cell.textLabel?.font = UIFont.boldSystemFontOfSize(18.0)
         cell.backgroundColor = UIColor.darkGrayColor()
         cell.textLabel?.textColor = UIColor(red: 5.0/255.0, green: 208.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        
         return cell
     }
     
@@ -57,9 +119,6 @@ class BarsNearbyView: UIViewController, UITableViewDataSource, UITableViewDelega
         self.performSegueWithIdentifier("BarsNearby", sender: self)
     }
     
-    func locationManger(manager: CLLocationManager){
-        
-    }
     
     func loginViewShowingLoggedOutUser(loginView: FBLoginView){
         self.performSegueWithIdentifier("MainScreen", sender : self)
